1cc4ff39d46d267642aa3e1293a32613414a8b4e
loja-apple-online <cancelamento5540@gmail.com>
2026-06-26 19:42:42 -0300

    Create products.js

---
+++ products.js
@@ -0,0 +1,51 @@
+const products = [
+{
+id: 1,
+name: "iPhone 13 128GB",
+price: 1480.96,
+image: "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-13-01.jpg",
+pix: "PIX_IPHONE_13"
+},
+{
+id: 2,
+name: "iPhone 15 128GB",
+price: 3149.37,
+image: "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-15-1.jpg",
+pix: "PIX_IPHONE_15_128"
+},
+{
+id: 3,
+name: "iPhone 15 256GB",
+price: 3499.37,
+image: "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-15-2.jpg",
+pix: "PIX_IPHONE_15_256"
+},
+{
+id: 4,
+name: "iPhone 16e 128GB",
+price: 2589.37,
+image: "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-14-pro-1.jpg",
+pix: "PIX_IPHONE_16E_128"
+},
+{
+id: 5,
+name: "iPhone 16e 256GB",
+price: 2869.37,
+image: "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-14-pro-2.jpg",
+pix: "PIX_IPHONE_16E_256"
+},
+{
+id: 6,
+name: "iPhone 17 Pro Max 256GB",
+price: 7118.37,
+image: "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-15-pro-max-1.jpg",
+pix: "PIX_IPHONE_17PM_256"
+},
+{
+id: 7,
+name: "iPhone 17 Pro Max 1TB",
+price: 9764.37,
+image: "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-15-pro-max-2.jpg",
+pix: "PIX_IPHONE_17PM_1TB"
+}
+];
