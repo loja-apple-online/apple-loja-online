f06a90330e8845c6169014d04a2e645f346952c6
loja-apple-online <cancelamento5540@gmail.com>
2026-06-26 19:55:38 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const app = document.getElementById("app");
+
+function render(){
+
+app.innerHTML = products.map(p=>`
+<div class="card">
+
+<img src="${p.image}">
+
+<h3>${p.name}</h3>
+
+<p>R$ ${p.price}</p>
+
+<button onclick="buy(${p.id})">
+Comprar agora
+</button>
+
+</div>
+`).join("");
+
+}
+
+function buy(id){
+const product = products.find(p=>p.id===id);
+localStorage.setItem("product", JSON.stringify(product));
+window.location.href = "checkout.html";
+}
+
+render();
